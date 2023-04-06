c16251e2c6b50f3606db53b3b1f3ed1d1ae22f45
lzappani <lzappani91@gmail.com>
2023-04-06 15:52:28 -0300

    Fourth commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -11,6 +10,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        // Primero, procesamos los argumentos
         if (args.length != 2) {
             System.err.println("Se deben proporcionar dos argumentos: la ruta del archivo CSV y el tipo de salida (JSON)");
             System.exit(1);
@@ -23,19 +23,24 @@ public class Main {
             System.exit(1);
         }
 
+        // Creamos la lista de ofertas
         List<Oferta> ofertas = new ArrayList<>();
 
+        // Leemos el archivo csv y creamos los objetos
         try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/" + csvFilePath))) {
             String line;
-            br.readLine();
+            br.readLine();  // Omitimos la primera línea de encabezado
             while ((line = br.readLine()) != null) {
                 String[] fields = line.split(",");
 
+                // Controlamos que la línea tenga 9 campos
                 if (fields.length != 9) {
                     System.err.println("Error: El archivo CSV no tiene el formato correcto");
                     System.exit(1);
                 }
                 Oferta oferta = null;
+
+                // En caso de que implementemos otros tipos de descuento, debemos colocarlos aquí
                 switch (fields[4]) {
                     case "%" -> oferta = new Oferta(
                             new Producto(fields[0],
@@ -54,7 +59,7 @@ public class Main {
                         System.exit(1);
                     }
                 }
-                ofertas.add(oferta);
+                ofertas.add(oferta);  // Agregamos la oferta a la lista
             }
         } catch (IOException e) {
             System.err.println("Error al leer el archivo CSV: " + e.getMessage());
@@ -67,18 +72,15 @@ public class Main {
             System.exit(1);
         }
 
+        // Creamos el Object Mapper
         ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);  // Esta línea realiza la indentación de la salida
 
         try {
-            ObjectNode rootNode = objectMapper.createObjectNode();
-            rootNode.putPOJO("ofertas", ofertas);
-            System.out.println(objectMapper.writeValueAsString(rootNode));
+            System.out.println(objectMapper.writeValueAsString(ofertas));  // Imprimimos cada objeto de la lista ofertas en formato json
         } catch (IOException e) {
             System.err.println("Error al convertir las ofertas a JSON: " + e.getMessage());
             System.exit(1);
         }
-
-
     }
 }
